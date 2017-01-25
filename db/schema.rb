ab2769a61d8fcaaf66aabedf1cc13ae4d22ed478
Katie Dunayczan <katie.dunayczan@gmail.com>
2017-01-25 15:28:43 -0500

    create user table, migration, and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117192555) do
+ActiveRecord::Schema.define(version: 20170125201633) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170117192555) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
