ea27bfa994a6a5310cec3b5f58755ff52b1903a5
Katie Dunayczan <katie.dunayczan@gmail.com>
2017-01-25 16:33:58 -0500

    associations and debugger view

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
+  has_many :posts
+  before_save { self.email = email.downcase }
 	validates :username, 
 		presence: true, 
 		uniqueness: { case_sensitive: false }, 
 		length: { minimum: 3, maximum: 25 }
-	VALID_EMAIL_REGEX = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
+	VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, 
 		presence: true, 
 		uniqueness: {case_sensitive: false}, 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125201633) do
+ActiveRecord::Schema.define(version: 20170125210403) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170125201633) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
