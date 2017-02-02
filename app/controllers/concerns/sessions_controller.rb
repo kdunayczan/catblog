d3866653d6ffbedaed2d694b62267c54473895f8
Katie Dunayczan <katie.dunayczan@gmail.com>
2017-02-02 15:45:40 -0500

    session create method

--- app/controllers/concerns/sessions_controller.rb
+++ app/controllers/concerns/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
 
-def new
-end
+	def new
+	end
 
-def create 
-end
+	def create 
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id
+			flash.now[:success] = "You have successfully logged in"
+			redirect_to users_path(user)
+		else
+			flash.now[:danger] = "There was something wrong with you login information"
+			render 'new'
+		end
+	end
 
-def destroy
-end
+	def destroy
+		
+	end
 
 end
